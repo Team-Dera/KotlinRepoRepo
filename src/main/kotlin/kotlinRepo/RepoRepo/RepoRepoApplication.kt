0250dbef6cc0661bbda1d4ee99d0fb700a41177e
ilyoil2 <0211ilyoil@gmail.com>
2024-01-03 00:00:13 +0900

    init: project settings

---
+++ src/main/kotlin/kotlinRepo/RepoRepo/RepoRepoApplication.kt
@@ -0,0 +1,11 @@
+package kotlinRepo.RepoRepo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RepoRepoApplication
+
+fun main(args: Array<String>) {
+	runApplication<RepoRepoApplication>(*args)
+}
---
+++ src/test/kotlin/kotlinRepo/RepoRepo/RepoRepoApplicationTests.kt
@@ -0,0 +1,13 @@
+package kotlinRepo.RepoRepo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RepoRepoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
