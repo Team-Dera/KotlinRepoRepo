434f857a5f32689e941b3dff2cfa16cec2bcd7d8
ilyoil2 <0211ilyoil@gmail.com>
2024-01-08 22:40:19 +0900

    feat: annotations

---
+++ repo-core/src/main/kotlin/kotlinRepo/reporepo/common/annotation/Aggregate.java
@@ -0,0 +1,12 @@
+package kotlinRepo.reporepo.common.annotation;
+
+import kotlin.annotation.AnnotationRetention;
+import kotlin.annotation.AnnotationTarget;
+import kotlin.annotation.Retention;
+import kotlin.annotation.Target;
+import org.springframework.transaction.annotation.Transactional;
+
+@Retention(AnnotationRetention.RUNTIME)
+@Target(allowedTargets = AnnotationTarget.CLASS)
+public @interface Aggregate {
+}
